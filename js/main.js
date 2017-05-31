44cf29f67736a58e844ee379f965c2c03cc5c5d2
danieldauk <danieldauk@gmail.com>
2017-05-31 18:39:40 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var corners = ["#upLeft", "#upRight", "#downLeft", "#downRight"];
 var randomMoves = [];
 var userMoves = [];
-var movesCounter = 5;
+var movesCounter = 2;
+var stricMode = 0;
 
 $(function() {
     generateRandomMoves();
@@ -20,23 +21,28 @@ $(function() {
   
     startGame();
   function startGame() {
+    console.log("started");
     for(var i=0, delay=0; i<movesCounter; i++) {
       var corner = randomMoves[i];
-      showPattern(corner, delay);
+      showPattern(corner, delay, i);
       delay+=1000;
     }
-    //playerMove();
     
   };
   
   //showing pattern
-  function showPattern (move, delay) {
+  function showPattern (move, delay, count) {
     setTimeout(function(){
     $(move).addClass("active");
       }, delay);
     setTimeout(function(){
       $(move).removeClass("active");
     }, delay+800);
+    if(count +1 == movesCounter) {
+      setTimeout(function(){
+      playerMove();
+    }, delay+900);
+    }
   }
 
   //putting player moves into array
@@ -44,37 +50,42 @@ $(function() {
   function playerMove() {
     userMoves = [];
     var currentMove = -1;
+    $(".gamePart").on("mousedown", function() {
+      $(this).addClass("active");
+    });
+    $(".gamePart").on("mouseleave mouseup",function(){
+      $(this).removeClass("active");
+    });
+  
+
     $(".gamePart").on("click", function() {
       currentMove++;
       var move = $(this).attr("id");
       userMoves.push(move);
       
-      if(userMoves[currentMove] != randomMoves[currentMove]) {
-        //error start over or strict?
-      } else if(/*function check for*/) {
-        movesCounter++;
-        startGame();
-      } else {
-        //wrong user pattern
-        $(".gamePart").off("click");
-        //repeat?
+      //if current input is not as randomPattern
+      if("#" + userMoves[currentMove] != randomMoves[currentMove]) {
+        //if strict mode is on
+        if(strictMode = 1) {
+          $(".gamePart").off();
+          $("body").css("background-color", "red");
+          movesCounter = 1;
+          startGame();
+        } else {
+          $(".gamePart").off();
+          $("body").css("background-color", "red");
+          startGame();
+        }
+      //if all moves were correct
+      } else if(userMoves.length == movesCounter) {
+          $(".gamePart").off();
+          movesCounter++;
+          startGame();
       }
-        
     });
     
   };
   
-
-//compare user pattern and random pattern
-  function comparePatterns () {
-    var currentRandomPattern = randomMoves.slice(0, movesCounter);
-    
-    for(var i = 0; i<randomMoves.length; i++) {
-      if(currentRandomPattern[i] != userMoves[i]) {
-        // return error: start over? strict?
-      }
-    }
-  };
 
 
   
